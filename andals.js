37c0f19ab664f446d92b61486cd9ca926ef5f8cf
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-05-28 15:10:47 +0530

    init

---
+++ andals.js
@@ -0,0 +1,23 @@
+'use strict';
+
+carbon.registerModule('carbon.andals');
+
+angular.module('carbon.andals', ['ui.router'])
+    .config(function($stateProvider) {
+        $stateProvider.state('andalsHome', {
+            url: '/andals',
+            templateUrl: 'components/carbon-andals/main.html',
+            controller: 'andalsCtrl',
+            permissions: ['andals'],
+            navigationBar: {
+                name: 'Andals',
+                glyphicon: 'cloud',
+                weight: 0
+            }
+        });
+    })
+    .controller('andalsCtrl', function ($scope, carbonApiProvider) {
+        $http.get(carbonApiProvider.url('/andalsSay')).success(function (data) {
+            $scope.apiSays = data;
+        });
+    });
